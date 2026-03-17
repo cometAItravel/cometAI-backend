b60d952b82950a5be2767b4e4de2c5263c4f5633
Vishaal <dynamics.studyai@gmail.com>
2026-03-17 12:47:22 +0530

    Fixed WhatsApp bot urlencoded

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
